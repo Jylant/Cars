ed18e35d4afd6bc3823df79ba9d17e3ee01c0cf5
jylant <jylhant@gmail.com>
2023-03-05 22:06:34 +0200

    toimivat tietokannat ja CRUDit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Cars
 Rästitehtävä
+
+create table car(
+id_car int primary key auto_increment,
+branch varchar(20),
+model varchar(20)
+);
+
+insert into car(branch,model) values('Toyota','Avensis');
+insert into car(branch,model) values('Ford','Focus');
+insert into car(branch,model) values('Lada','Samara');
---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const bookRouter = require('./routes/book');
+const carRouter = require('./routes/car');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors());
+app.use('/book', bookRouter);
+app.use('/car', carRouter);
+
+
+module.exports = app;
